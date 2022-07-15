3a8fa063d7fdb04d59de38bfb8769e9f272e46cc
김영목 <mokyoungg@gmail.com>
2022-07-15 14:42:15 +0900

    feat: create TextInput Component

--- src/components/SignupForm/SignupForm.style.ts
+++ src/components/SignupForm/SignupForm.style.ts
@@ -1,7 +1,22 @@
 import styled from 'styled-components';
 
+const black = '#242427';
+
 export const FormContainer = styled.form`
   display: flex;
   flex-direction: column;
   gap: 16px;
+`;
+
+export const Button = styled.button`
+  width: 350px;
+  height: 30px;
+  padding: 5px 12px;
+  font-size: 14px;
+  background-color: #fff;
+  color: ${black};
+  outline: none;
+  border: 1px solid ${black};
+  border-radius: 4px;
+  cursor: pointer;
 `;
--- src/components/SignupForm/SignupForm.tsx
+++ src/components/SignupForm/SignupForm.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { Formik } from 'formik';
 import * as S from './SignupForm.style';
+import TextInput from 'components/TextInput/TextInput';
 
 const initialValue = {
   username: '',
@@ -10,17 +11,52 @@ const initialValue = {
 };
 
 const SignupForm: React.FC = () => {
+  const handleSubmit = useCallback((value) => {
+    console.log('value :', value);
+  }, []);
+
   return (
-    <Formik initialValues={initialValue} onSubmit={() => {}}>
+    <Formik initialValues={initialValue} onSubmit={handleSubmit}>
       {(formik) => {
-        const { values } = formik;
+        const { values, handleChange, handleSubmit } = formik;
 
         return (
-          <S.FormContainer>
-            <input value={values.username} />
-            <input value={values.email} />
-            <input value={values.password} />
-            <input value={values.confirmPassword} />
+          <S.FormContainer onSubmit={handleSubmit}>
+            <TextInput
+              id="username"
+              name="username"
+              value={values.username}
+              onChange={handleChange}
+              placeholder="Name"
+            />
+            <TextInput
+              id="email"
+              name="email"
+              type="email"
+              value={values.email}
+              onChange={handleChange}
+              placeholder="Email"
+            />
+            <TextInput
+              id="password"
+              name="password"
+              type="password"
+              value={values.password}
+              onChange={handleChange}
+              placeholder="Password"
+            />
+            <TextInput
+              id="confirmPassword"
+              name="confirmPassword"
+              type="password"
+              value={values.confirmPassword}
+              onChange={handleChange}
+              placeholder="Confirm Password"
+            />
+
+            <S.Button type="submit" onClick={() => {}}>
+              Submit
+            </S.Button>
           </S.FormContainer>
         );
       }}
